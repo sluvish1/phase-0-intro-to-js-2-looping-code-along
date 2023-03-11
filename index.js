8c4ef71a7a6f71b6321e50d5d985da4197eb38de
Vaschell McKenzie <mvaschell@gmail.com>
2023-03-11 09:30:36 -0500

    for loop while loop

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const names = [ 'Guadalupe', 'Ollie', 'Aki' ]/*names to be passed*/
+const messages =[]/*empty  Array that needs to be returned*/
+
+
+function writeCards(names, events) {
+    for (let i = 0; i < names.length; i++){
+      let nameMessage = `Thank you, ${names[i]}, for the wonderful ${events} gift!`;
+    messages.push(nameMessage); /*put the empty Array and the message with the names to be
+      passed together to make the new Array*/
+  }
+  return messages; /*prints out the message*/
+}
+
+function countDown(){
+let countDown = 0
+while (countDown < 11) {
+   console.log(countDown++);
+}
+}
